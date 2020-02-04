6032d426601e83851d7c2f563c6758dcadff08a1
Diego Linhares <Dieico@LME1109E004.ufcvirtual.local>
2020-02-04 11:51:46 -0300

    4

---
+++ src/components/pages/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/header.css';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>
+                Titulo
+            </h1>
+        </header>
+    );
+}
